92e937cd0084fb68d530f8dd49607a5d8a1a2167
sekitch42 <sarah.kitchen@ontariotechu.net>
2025-11-23 01:12:03 -0500

    more .env issues fixed

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,4 @@
-// localhost mongodb for testing
-
-const mongoose = require('mongoose');
-
-async function connectDB() {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/Fishdex", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB connected ✔");
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1);
-  }
-}
-
-module.exports = connectDB;
+// using .env and gitignore to hide mongodb password
+module.exports = {
+  URI: process.env.MONGO_URI
+};
